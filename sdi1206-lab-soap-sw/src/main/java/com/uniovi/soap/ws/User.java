76b45663a77b4fa8c861ff47484312ee6e2be000
UO247346 <uo247346@uniovi.es>
2019-04-22 21:07:40 +0200

    Mira que bien, ahora si se creo la carpeta soap.ws

--- sdi1206-lab-soap-sw/src/main/java/com/uniovi/soap/ws/User.java
+++ sdi1206-lab-soap-sw/src/main/java/com/uniovi/soap/ws/User.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2019.04.22 a las 09:03:29 PM CEST 
+// Generado el: 2019.04.22 a las 09:04:57 PM CEST 
 //
 
 
